87caefa4c309b591e81f8ba45e306b1e83d8b39d
Tracy Velasquez <velasqueztracy25@gmail.com>
2020-10-03 18:21:30 -0500

    mini tax calc

--- js/variables.js
+++ js/variables.js
@@ -10,17 +10,16 @@ let shipping;
 console.log("date is:" + date)
 
 
-
 let quantity;
 quantity = 4;
-let lowStock  = (quantity <= 5);
+let lowStock = (quantity <= 5);
 let lowStockMessage = "Order Soon! There are only 5 of this shirt left!"
 
 
 // One way
-if (lowStock){
+if (lowStock) {
     console.log("Order Soon! There are only 5 of this shirt left!")
-    }
+}
 
 // Alt way
 function lowStockChecker() {
@@ -30,3 +29,15 @@ function lowStockChecker() {
 }
 
 console.log(lowStockChecker());
+
+let itemPrice = 600;
+let taxCalc = itemPrice * 0.0825;
+
+function calculateTax() {
+    return taxCalc;
+}
+
+let total = itemPrice + calculateTax();
+
+console.log("Your tax is: " + calculateTax());
+console.log("Your total is: " + total);
