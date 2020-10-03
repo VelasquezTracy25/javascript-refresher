9ddf35d60233623f743bdfb6da14ab2239d59378
Tracy Velasquez <velasqueztracy25@gmail.com>
2020-10-03 18:07:07 -0500

    baby refresher on super basic things

---
+++ js/arrays.js
@@ -0,0 +1,8 @@
+var colors;
+colors = ["blue","green","yellow"];
+colors = ["red", "pink", "orange"];
+
+console.log(colors)
+
+let numbers = new Array(1,2,3,4,5);
+console.log("numbers array: " + numbers);
---
+++ js/variables.js
@@ -0,0 +1,12 @@
+let date = new Date();
+//new date object
+
+let firstName = "Tracy",
+    lastName = "Velasquez";
+
+let inStock;
+let shipping;
+
+console.log("date is:" + date)
+
+
